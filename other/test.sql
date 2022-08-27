3746cb6428fe997f7a96981c0e1eb812d4361902
TomasBerrojalviz <cai.berrojalviz.tomas@gmail.com>
2022-08-27 15:08:51 -0300

    update base

--- other/test.sql
+++ other/test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-08-2022 a las 18:53:25
+-- Tiempo de generación: 27-08-2022 a las 20:08:00
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `test`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autos`
+--
+
+CREATE TABLE `autos` (
+  `id` int(11) NOT NULL,
+  `id_estado` int(11) NOT NULL,
+  `patente` text NOT NULL,
+  `id_modelo` int(11) NOT NULL,
+  `anio` text NOT NULL,
+  `id_cliente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `autos`
+--
+
+INSERT INTO `autos` (`id`, `id_estado`, `patente`, `id_modelo`, `anio`, `id_cliente`) VALUES
+(1, 0, 'HDC940', 1, '2009', 1),
+(4, 1, 'ASD123', 32, '2020', 1),
+(5, 2, 'QWE', 3, '123', 8),
+(6, 5, 'QAZ', 2, '456', 8),
+(7, 3, 'HDP666', 33, '2001', 10),
+(8, 3, 'QWE123', 31, '2003', 10);
 
 -- --------------------------------------------------------
 
@@ -109,9 +136,45 @@ INSERT INTO `modelos` (`id`, `id_marca`, `modelo`) VALUES
 (32, 29, 'ONIX'),
 (33, 36, 'VENTO');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ordenes`
+--
+
+CREATE TABLE `ordenes` (
+  `id` int(11) NOT NULL,
+  `id_auto` int(11) NOT NULL,
+  `fecha_recibido` date NOT NULL DEFAULT current_timestamp(),
+  `problema` text NOT NULL,
+  `notas` text DEFAULT NULL,
+  `adjuntos` int(11) DEFAULT NULL,
+  `id_recibo` int(11) NOT NULL,
+  `id_presupuesto` int(11) NOT NULL,
+  `solucion` text DEFAULT NULL,
+  `fecha_devolucion` date DEFAULT NULL,
+  `estado` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `ordenes`
+--
+
+INSERT INTO `ordenes` (`id`, `id_auto`, `fecha_recibido`, `problema`, `notas`, `adjuntos`, `id_recibo`, `id_presupuesto`, `solucion`, `fecha_devolucion`, `estado`) VALUES
+(3, 1, '2022-08-27', 'QWE', 'ASD', NULL, 0, 0, NULL, NULL, 1),
+(4, 1, '2022-08-27', 'PROBLEMAS CON LA CORREA', 'REVISAR X COSA', NULL, 0, 0, NULL, NULL, 1),
+(5, 4, '2022-08-27', 'QUILOMBO CON EL ONIX', 'TEST', NULL, 0, 0, NULL, NULL, 1),
+(6, 8, '2022-08-27', 'AUTO GIANE', '', NULL, 0, 0, NULL, NULL, 1);
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `autos`
+--
+ALTER TABLE `autos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `clientes`
@@ -132,8 +195,20 @@ ALTER TABLE `modelos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `ordenes`
+--
+ALTER TABLE `ordenes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `autos`
+--
+ALTER TABLE `autos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `clientes`
@@ -152,6 +227,12 @@ ALTER TABLE `marcas`
 --
 ALTER TABLE `modelos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT de la tabla `ordenes`
+--
+ALTER TABLE `ordenes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
