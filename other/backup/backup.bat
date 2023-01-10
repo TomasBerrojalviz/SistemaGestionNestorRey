ab1c83525660cab4e44b81aef4bd0ce7fcead05d
TomasBerrojalviz <cai.berrojalviz.tomas@gmail.com>
2023-01-09 23:49:15 -0300

    Se agrega fecha al backup

--- other/backup/backup.bat
+++ other/backup/backup.bat
@@ -1,3 +1,3 @@
 echo off
-C:\xampp\mysql\bin\mysqldump -h127.0.0.1 -ubackup -pbackup sgnr > C:\xampp\htdocs\SistemaGestionNestorRey\other\backup\sgnrDB_BackUp.sql
+C:\xampp\mysql\bin\mysqldump -h127.0.0.1 -ubackup -pbackup sgnr > C:\xampp\htdocs\SistemaGestionNestorRey\other\backup\sgnrDB_BackUp_%date:~-10,2%-%date:~-7,2%-%date:~-4,4%.sql
 exit
